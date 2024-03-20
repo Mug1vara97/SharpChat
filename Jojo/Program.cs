503eaa4a78d494fd5e3744b6c2275a96e62930e4
Mug1vara97 <mugivarakun97@gmail.com>
2024-03-20 13:29:37 +0300

    Добавьте файлы проекта.

---
+++ Jojo/Program.cs
@@ -0,0 +1,34 @@
+using SignalRApp;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSignalR();
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseSession();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Login}/{id?}");
+
+app.MapHub<ChatHub>("/chat");
+
+app.Run();
