6fe307df58a16f566f7e360f618c7ce209fc9b7b
Mug1vara97 <mugivarakun97@gmail.com>
2024-04-19 19:16:46 +0300

    1

--- Jojo/Controllers/HomeController.cs
+++ Jojo/Controllers/HomeController.cs
@@ -39,9 +39,11 @@ namespace Jojo.Controllers
             var userChats = chats.Where(c => c.AllowedUsers.Contains(username) || c.AllowedUsers.Contains("public")).ToList();
 
             ViewBag.Chats = userChats;
+            ViewBag.UnreadMessageCounts = GetUnreadMessageCount(username);
 
             return View();
         }
+
 
 
         [HttpGet]
@@ -58,14 +60,20 @@ namespace Jojo.Controllers
             var chat = _context.Chats.Find(chatId);
             if (chat != null)
             {
-                ViewBag.ChatName = chat.Name;
+                if (chat.AllowedUsers.Contains(username) || chat.AllowedUsers.Contains("public"))
+                {
+                    ViewBag.ChatName = chat.Name;
+
+                    var messages = _context.ChatMessages.Where(m => m.ChatId == chatId).ToList();
+                    ViewBag.Messages = messages;
+
+                    return View();
+                }
             }
 
-            var messages = _context.ChatMessages.Where(m => m.ChatId == chatId).ToList();
-            ViewBag.Messages = messages;
-
-            return View();
+            return RedirectToAction("ChatList", new { username });
         }
+
         [HttpPost]
         public async Task<IActionResult> CreateChat(string chatName, string chatDescription, string username, string addUsersType, string allowedUsers)
         {
@@ -129,7 +137,7 @@ namespace Jojo.Controllers
         }
 
         [HttpPost]
-       public async Task<IActionResult> Register(User user, string confirmPassword)
+        public async Task<IActionResult> Register(User user, string confirmPassword)
         {
             if (ModelState.IsValid)
             {
@@ -221,7 +229,7 @@ namespace Jojo.Controllers
                 return RedirectToAction("Login");
             }
 
-            var newsFeedItems = await _context.NewsFeedItems.Include(n => n.Comments).ToListAsync();
+            var newsFeedItems = await _context.NewsFeedItems.Include(n => n.Comments).Include(n => n.Likes).ToListAsync();
 
 
             ViewBag.Username = username;
@@ -314,9 +322,18 @@ namespace Jojo.Controllers
 
             ViewBag.UserNewsItems = userNewsItems;
 
+            var userFriends = _context.Friends.Where(f => (f.User1 == profilename || f.User2 == profilename)).ToList();
+            ViewBag.Friends = userFriends;
+
+            var userIsFriend = _context.Friends.Any(f => (f.User1 == username && f.User2 == profilename) || (f.User1 == profilename && f.User2 == username));
+            ViewBag.IsFriend = userIsFriend;
+            ViewBag.FriendshipRequests = _context.FriendshipRequests.ToList();
+
+
 
             return View(user);
         }
+
         [HttpPost]
         public async Task<IActionResult> EditProfile(string username, string profileDescription, IFormFile profilePhoto, string backgroundColor)
         {
@@ -362,10 +379,134 @@ namespace Jojo.Controllers
                 await _context.SaveChangesAsync();
             }
 
-            // Возвращаем JSON с успешным результатом
             return Json(new { success = true });
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddFriend(string username, string friendUsername)
+        {
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(friendUsername))
+            {
+                return BadRequest();
+            }
+
+            var friendshipExists = _context.Friends.Any(f => (f.User1 == username && f.User2 == friendUsername) || (f.User1 == friendUsername && f.User2 == username));
+            if (friendshipExists)
+            {
+                return BadRequest("Friendship already exists");
+            }
+
+            var newFriendship = new Friendship { User1 = username, User2 = friendUsername };
+            _context.Friends.Add(newFriendship);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Profile", new { username = username, profilename = friendUsername });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RemoveFriend(string username, string friendUsername)
+        {
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(friendUsername))
+            {
+                return BadRequest();
+            }
+
+            var friendship = _context.Friends.FirstOrDefault(f =>
+                (f.User1 == username && f.User2 == friendUsername) || (f.User1 == friendUsername && f.User2 == username));
+
+            if (friendship != null)
+            {
+                _context.Friends.Remove(friendship);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction("Profile", new { username = username, profilename = friendUsername });
+        }
+        [HttpPost]
+        public async Task<IActionResult> LikeNews(int newsItemId, string username)
+        {
+            var existingLike = _context.Likes.FirstOrDefault(l => l.NewsFeedItemId == newsItemId && l.Username == username);
+
+            if (existingLike == null)
+            {
+                var newLike = new Like { NewsFeedItemId = newsItemId, Username = username };
+                _context.Likes.Add(newLike);
+            }
+            else
+            {
+                _context.Likes.Remove(existingLike);
+            }
+
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UnlikeNews(int newsItemId, string username)
+        {
+            var existingLike = _context.Likes.FirstOrDefault(l => l.NewsFeedItemId == newsItemId && l.Username == username);
+
+            if (existingLike != null)
+            {
+                _context.Likes.Remove(existingLike);
+                await _context.SaveChangesAsync();
+            }
+
+            return Ok();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SendFriendRequest(string fromUser, string toUser)
+        {
+            var existingRequest = _context.FriendshipRequests.FirstOrDefault(fr => fr.FromUser == fromUser && fr.ToUser == toUser);
+
+            if (existingRequest == null)
+            {
+                var newRequest = new FriendshipRequest { FromUser = fromUser, ToUser = toUser, IsAccepted = false };
+                _context.FriendshipRequests.Add(newRequest);
+                await _context.SaveChangesAsync();
+
+                return Ok("Friend request sent");
+            }
+
+            return BadRequest("Friend request already sent");
         }
 
 
+        [HttpPost]
+        public async Task<IActionResult> AcceptFriendRequest(int requestId)
+        {
+            var request = _context.FriendshipRequests.Find(requestId);
+
+            if (request != null)
+            {
+                request.IsAccepted = true;
+                _context.FriendshipRequests.Remove(request);
+
+                var newFriendship = new Friendship { User1 = request.FromUser, User2 = request.ToUser };
+                _context.Friends.Add(newFriendship);
+
+                await _context.SaveChangesAsync();
+
+                return Ok("Friend request accepted");
+            }
+
+            return NotFound();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> RejectFriendRequest(int requestId)
+        {
+            var request = _context.FriendshipRequests.Find(requestId);
+
+            if (request != null)
+            {
+                _context.FriendshipRequests.Remove(request);
+                await _context.SaveChangesAsync();
+
+                return Ok("Friend request rejected");
+            }
+
+            return NotFound();
+        }
     }
 }
